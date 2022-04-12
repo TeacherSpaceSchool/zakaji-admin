f9ce1ee80eb83245522a034d6c4e5e8816ae1790
TeacherSpaceSchool <azyk.store@gmail.com>
2022-04-13 00:32:11 +0600

    check

--- pages/agentroute/[id].js
+++ pages/agentroute/[id].js
@@ -109,7 +109,6 @@ const AgentRoute = React.memo((props) => {
                 else if (selectType == 'Свободные')
                     allClient=district.client.filter(client=>!clients[dayWeek].includes(client._id))
                 else if (selectType == 'Выбраные') {
-                    console.log(clients[dayWeek])
                     allClient = clients[dayWeek].map(client=>district.client.find(client1=>client1._id===client))
                 }
                 let filtredClient = [...allClient]
